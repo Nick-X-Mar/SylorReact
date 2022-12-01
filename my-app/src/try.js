532ce5418f0ed678c91ff4226cffe33ea9ee856b
Nick-X-Mar <nmarianos93@gmail.com>
2022-12-01 17:42:25 +0200

    changes

--- my-app/src/try.js
+++ my-app/src/try.js
@@ -7,7 +7,7 @@ function Nikos(){
     useEffect(() => {
         fetch("/users", {
             method: 'GET',
-            headers: { 'x_access_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im5pa29zIiwiZXhwIjoxNjY5ODg2MjM0fQ.v_4liAIP7g8cdkZozq8HiWnau48kX6AIZ4IzUVAxSIY',
+            headers: { 'x_access_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im5pa29zIiwiZXhwIjoxNjY5OTk1NTQ5fQ.s4ZogB0--5uhZGMp6Yx2MT9FQM0DvnUSqaFLLI70aSw',
             'Content-Type': 'application/json',
             // 'Access-Control-Allow-Origin': 'http://localhost:3000/initialization'
         }
