15e202309a0c50b7ae46943a7a973c8da44ac4b5
Nick-X-Mar <nmarianos93@gmail.com>
2022-12-17 00:28:17 +0200

    Delete

--- my-app/src/dataprovider.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const MyDataProvider = new React.createContext({Users: [], setUsers: () => {}, Products: [], setProducts: () => {}}); // empty js array //Provider
-
-export default MyDataProvider;
